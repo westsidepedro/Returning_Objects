f94729c23daf02e97e8d17ef9c6017ede279e184
jpetterson <jpetterson@DS-17.decker.local>
2018-02-28 11:54:23 -0600

    2/28/18

---
+++ src/Returning_Objects.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Returning_Objects.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello there Sexy!" << endl; // prints Hello there Sexy!
+	return 0;
+}
